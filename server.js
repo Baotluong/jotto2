d1dd09ecefd046a3d8929a8b568de50a7060784f
Bao Luong <Bao@Baos-MacBook-Pro.local>
2016-12-19 12:17:37 -0500

    messing with port

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var app = express();
 var mongojs = require('mongojs');
 var db = mongojs('jotto2', ['jotto2']);
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
-app.listen(port);
-console.log("server running from port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 app.post('/newgame', function (req, res) {
 		
